3a55f71f6e17cf6068f33f556bb5d1c496c8b6d7
Victor Marotta <victordossantosmarotta@gmail.com>
2024-07-09 08:15:33 -0300

    feat: add focos layer

--- src/components/OLMap.tsx
+++ src/components/OLMap.tsx
@@ -1,8 +1,11 @@
-import { RLayerTile, RMap, ROSM } from "rlayers";
+import { RLayerTile, RLayerVectorTile, RMap, ROSM, RStyle } from "rlayers";
 import { useApp } from "../providers/AppProvider";
 import { RView } from "rlayers/RMap";
+import MVT from "ol/format/MVT";
 import "ol/ol.css";
 import { fromLonLat, toLonLat } from "ol/proj";
+import { RCircle, RFill, RStroke } from "rlayers/style";
+import { theme } from "../theme";
 
 export default function OLMap() {
   const { viewMetadata, setViewMetadata } = useApp();
@@ -31,14 +34,23 @@ export default function OLMap() {
       view={[viewState, setViewState]}
       noDefaultControls
     >
-      <RLayerTile
+      {/* <RLayerTile
         url={"http://basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"}
-        zIndex={10}
-      />
+      /> */}
       <RLayerTile
         url={"http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}"}
-        zIndex={11}
       />
+      <RLayerVectorTile
+        url={"https://tiles.marotta.dev/data.focos/{z}/{x}/{y}.pbf?"}
+        format={new MVT()}
+      >
+        <RStyle.RStyle>
+          <RStroke color={"white"} width={3} />
+          <RCircle radius={6}>
+            <RFill color={theme.colors.brand["green400"]} />
+          </RCircle>
+        </RStyle.RStyle>
+      </RLayerVectorTile>
     </RMap>
   );
 }
